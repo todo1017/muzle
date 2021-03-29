b061819623ac2d5ae78a013b4c7c18229b9a925a
scriptmastership <todo1017@outlook.com>
2021-03-29 12:33:42 +0800

    config log

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'errorlog'],
+            'channels' => ['single', 'stderr','syslog','errorlog'],
             'ignore_exceptions' => false,
         ],
 
